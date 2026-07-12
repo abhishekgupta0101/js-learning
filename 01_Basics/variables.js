ecef1debd567e2af87e9fc537d0ab3cb7fcd1d14
abhishekgupta0101 <abhisheklenovo100@gmail.com>
2026-07-12 16:01:13 +0000

    update the code

---
+++ 01_Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 45454545;
+let accountEmail = "abhishek@gmail.com"
+var accountPassword = "12345678"
+accountcity = "Mumbai"
+
+// accountId = "91919191"
+accountEmail = "abc@gamil.com"
+accountPassword = "12345"
+accountcity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountcity]);
